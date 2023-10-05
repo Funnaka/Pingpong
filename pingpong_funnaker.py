21c179a796a08a660c248cc27fab9b12ab8b2fc0
Funnaka <121733041+Funnaka@users.noreply.github.com>
2023-10-05 18:55:21 +0300

    Add files via upload

--- pingpong_funnaker.py
+++ pingpong_funnaker.py
@@ -1,5 +1,5 @@
 from pygame import *
-
+from random import *
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
@@ -17,13 +17,13 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
 win_width = 600
@@ -41,13 +41,35 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+win_player1 = GameSprite('player1.jpg', 0, 0, 0, win_width, win_height)
+win_player2 = GameSprite('player2.jpg', 0, 0, 0, win_width, win_height)
+
+
+speed_x = 3
+speed_y = 3
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
-        background.reset()
 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= randint(-13, -7) / 10
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            win_player2.reset()
+            continue
+        if ball.rect.y < 0:
+            finish = True
+            win_player1.reset()
+            continue
+        background.reset()
+      
         racket1.update_l()
         racket2.update_r()
 
